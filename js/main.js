e883d396e0a4998796700924d1f48eaefe3a88b7
Enio Nicael <enionicael@gmail.com>
2018-05-11 15:25:01 -0300

    Feat: Implementar a filtragem
    Implementar o filtro atraves do menu dropdown

--- js/main.js
+++ js/main.js
@@ -1,143 +1,179 @@
 // dados locais
-
 var locations = [
   {
-    title: 'EM Francisco Meireles',
+    title: 'Itaipu Binacional',
     location: {
-      lat: -22.197116,
-      lng: -54.787947
+      lat: -25.446952,
+      lng: -54.584818
     }
   },
   {
-    title: 'Hospital da Missâo',
+    title: 'Vale dos Dinossauros',
     location: {
-      lat: -22.195912,
-      lng: -54.788572}
+      lat: -25.590745,
+      lng: -54.518337}
   },
   {
-    title: 'Posto de saúde',
+    title: 'Dreamland Museu de Cera Foz do Iguaçu',
     location: {
-      lat: -22.183608,
-      lng: -54.797779}
+      lat: -25.591244,
+      lng: -54.516539}
   },
   {
-    title: 'EMI Ramão Martins',
+    title: 'Parque das Aves',
     location: {
-      lat: -22.17732,
-      lng: -54.807836}
+      lat: -25.613505,
+      lng: -54.482524}
   },
   {
-    title: 'EMI Tengatui Marangatu',
+    title: 'Cataratas do Iguaçu',
     location: {
-      lat: -22.174244,
-      lng: -54.828739}
-  },
-  {
-    title: 'EE Marçal de Souza',
-    location: {
-      lat: -22.174482,
-      lng: -54.829439}
-  },
-  {
-    title: 'Posto de saúde',
-    location: {
-      lat: -22.174183,
-      lng: -54.829023}
-    },
-  {
-    title: 'Vila Olimpica',
-    location: {
-      lat: -22.169899,
-      lng: -54.837355}
-    }
+      lat: -25.691719,
+      lng: -54.435274}
+  }
 ];
-
 // var markers = [];
-
-var Location = function(data, map) {
+var Location = function(data, marker) {
   this.name = ko.observable(data.title);
   this.location = ko.observable(data.location);
-  this.marker = ko.observable(new google.maps.Marker({
-    position: data.location,
-    title: data.title,
-    map: map,
-    animation: google.maps.Animation.DROP
-    // icon: this.defaultIcon,
-    // id: i
-  })
-  );
+  this.marker = ko.observable(marker);
 };
 
 var ViewModel = function(locations, map){
 
-  // this.defaultIcon = this.makeMarkerIcon('0091ff');
-  // this.highlightedIcon = this.makeMarkerIcon('FFFF24');
-  // this.largeInfowindow = new google.maps.InfoWindow();
+  var self = this;
+  // this.defaultIcon = self.makeMarkerIcon('0091ff');
+  // self.highlightedIcon = self.makeMarkerIcon('FFFF24');
 
-  this.locationList = ko.observableArray([]); // lista dos locais
-  this.locationMarkers = ko.observableArray([]); // lista de marcadores dos locais
+  self.largeInfowindow = ko.observable(new google.maps.InfoWindow());
+  self.bounds = ko.observable(new google.maps.LatLngBounds());
+
+  self.locationList = ko.observableArray([]); // lista dos locais
+  self.locationMarkers = ko.observableArray([]); //  lista de marcadores dos locais
+  self.visibleMarkers = ko.observableArray([]); // locais visiveis
 
   // popula o array com os marcadores dos locais
-  // for (var i = 0; i < locations.length; i++) {
-  //   this.locationMarkers()[i] = new google.maps.Marker({
-  //     position: locations[i].location,
-  //     title: locations[i].title,
-  //     map: map,
-  //     animation: google.maps.Animation.DROP
-      // icon: this.defaultIcon,
+  for (var i = 0; i < locations.length; i++) {
+    self.locationMarkers()[i] = new google.maps.Marker({
+      position: locations[i].location,
+      title: locations[i].title,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      // icon: self.defaultIcon,
       // id: i
-  //   });
-  // }
+    });
 
+    self.bounds().extend(self.locationMarkers()[i].position);
+
+    self.locationMarkers()[i].addListener('click', function() {
+      self.populateInfoWindow(this, self.largeInfowindow());
+    });
+
+    // self.locationMarkers()[i].addListener('mouseover', function(){
+    //   self.setIcon(self.highlightedIcon);
+    // });
+    // self.locationMarkers()[i].addListener('mouseout', function(){
+    //   self.setIcon(self.defaultIcon);
+    // });
+  }
+
+  map.fitBounds(self.bounds());
 
   // popula o array com a lista dos locais
   for (var i = 0; i < locations.length; i++) {
-    this.locationList()[i] = new Location(locations[i], map);
-    console.log('locationList: ' + this.locationList()[i].name());
+    self.locationList()[i] = new Location(locations[i], self.locationMarkers()[i]);
+    // console.log('locationList: ' + self.locationList()[i].name());
+
+    // self.locationList()[i].marker().addListener('click', function(){
+    //   self.populateInfoWindow(self, largeInfowindow);
+    // });
   }
 
   // locations.forEach(function(){
-  //   this.locationList.push('Ola');
+  //   self.locationList.push('Ola');
   // });
 
-  // console.log('locationList: '+this.locationList()[0].location().lat);
+  // console.log('locationList: '+self.locationList()[0].location().lat);
 
 
   // for (var i = 0; i < locations.length; i++) {
-  //   this.locationList()[i] = new Location(locations[i], this.locationMarkers()[i]);
+  //   self.locationList()[i] = new Location(locations[i], self.locationMarkers()[i]);
   // }
 
-  // this.showListings = function() {
+  // self.showListings = function() {
     // var bounds = new google.maps.LatLngBounds();
 
-    // for (var i = 0; i < this.locationList().length; i++) {
-    //   this.locationList()[i].marker.setMap(map);
-      // bounds.extend(this.locationList()[i].marker.position);
-      // console.log('locationList: ' + this.locationList()[i].marker.title);
+    // for (var i = 0; i < self.locationList().length; i++) {
+    //   self.locationList()[i].marker.setMap(map);
+      // bounds.extend(self.locationList()[i].marker.position);
+      // console.log('locationList: ' + self.locationList()[i].marker.title);
     // }
 
     // map.fitBounds(bounds);
   // };
 
-/*
+
 
   // eventos
-  this.makeMarkerIcon = function(markerColor) {
-    var markerImage = new google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-      '|40|_|%E2%80%A2',
-      new google.maps.Size(21, 34),
-      new google.maps.Point(0, 0),
-      new google.maps.Point(10, 34),
-      new google.maps.Size(21,34));
-    return markerImage;
-  };
+  // self.makeMarkerIcon = function(markerColor) {
+  //   var markerImage = new google.maps.MarkerImage(
+  //     'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+  //     '|40|_|%E2%80%A2',
+  //     new google.maps.Size(21, 34),
+  //     new google.maps.Point(0, 0),
+  //     new google.maps.Point(10, 34),
+  //     new google.maps.Size(21,34));
+  //   return markerImage;
+  // };
 
+
+  self.locationList().forEach(function(place){
+    self.visibleMarkers.push(place);
+  });
+  // interage com o evento click da lista
+  self.filterMarkers = function(){
+    // var location = self.visibleMarkers.name;
+    // visibleMarkers.forEach(function(mark){
+    //
+    // });
+    // if (place.) {
+    //
+    // }
+    // console.log('local capturado pelo evento: '+ location);
+  //   console.log('click !');
+  }
   // InfoWindow
-  this.populateInfoWindow = function(marker, infowindow) {
+  self.populateInfoWindow = function(marker, infowindow) {
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
     if (infowindow.marker != marker) {
       infowindow.marker = marker;
-      infowindow.setContent('<div>'+marker.title+'</div>');
+      // usar o setTimeout para tratamento de erros
+      var wikiRequestTimeout = setTimeout(function(){
+        infowindow.setContent('<div><p>Não foi possivel acessar dados do wikipedia!</p>,</div>');
+      }, 8000);
+      // faz uma requisiçao ajax na API do wikipedia
+      $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        success: function(response){
+          var articleList = response[1];
+
+          if ($.isEmptyObject(articleList)) {
+            infowindow.setContent('A pesquisa não encontrou nenhum resultado.');
+          }else {
+            for(var i = 0; i < articleList.length; i++){
+              articleStr = articleList[i];
+              var url = 'https://en.wikipedia.org/wiki/' + articleStr;
+              infowindow.setContent('<div>'+marker.title+'</div>'+
+                                    '<div><ul><li><a href=" ' + url + '" target="_blank">' + articleStr +
+                                    '</a></li></ul></div>');
+            };
+          }
+
+          clearTimeout(wikiRequestTimeout);
+        }
+      });
+
       infowindow.open(map, marker);
       infowindow.addListener('closeclick', function(){
         infowindow.marker = null;
@@ -145,20 +181,18 @@ var ViewModel = function(locations, map){
     }
   };
 
-  this.filterMarkers = function() {
-    if (true) {
-
-    }
-  }; */
+  // self.filterMarkers = function() {
+  //   if (true) {
+  //
+  //   }
+  // };
 }
 
 var initMap = function(){
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -22.177092, lng: -54.808242},
-    // center: {lat: -21.128627, lng: -56.492892},
+    center: {lat:  -25.516336, lng: -54.585376},
     zoom: 15
   });
-
 
   ko.applyBindings(new ViewModel(locations, map));
 };
